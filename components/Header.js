633e80b5d9183b54b1851c28e48064f4fb4fcd57
john <jaservo@up.edu.ph>
2022-12-24 17:26:56 +0800

    feat: header component

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Hello! Welcome to my Blog.</h1>
+      <h2>This is a practice website to test my NextJS skills.</h2>
+    </header>
+  );
+};
+
+export default Header;
