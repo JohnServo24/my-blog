eefcd0868e1f4df8d78883556f9c8fa6b2a35a56
john <jaservo@up.edu.ph>
2022-12-25 15:39:55 +0800

    feat: add css to header

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
+import HeaderStyles from "../styles/Header.module.css";
+
 const Header = () => {
   return (
-    <header>
+    <header className={HeaderStyles.header}>
       <h1>Hello! Welcome to my Blog.</h1>
       <h2>This is a practice website to test my NextJS skills.</h2>
     </header>
