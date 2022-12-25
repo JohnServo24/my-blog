5f63a0a00eb47d91ac5687f2716ae7a38224f1ba
john <jaservo@up.edu.ph>
2022-12-25 15:19:06 +0800

    feat: blogs now have their own pages

--- pages/blog/[id]/index.js
+++ pages/blog/[id]/index.js
@@ -1,18 +1,39 @@
-const blog = () => {
-  return <div>index</div>;
+const blog = ({ blog }) => {
+  return (
+    <main>
+      <h1>{blog.title}</h1>
+      <p>{blog.body}</p>
+    </main>
+  );
 };
 
 export const getStaticProps = async (context) => {
   const baseUrl = process.env.BASE_URL;
-  const res = await fetch(`${baseUrl}/api/blog`);
-  const blogs = await res.json();
+  const blogID = context.params.id;
 
-  console.log(context);
+  const res = await fetch(`${baseUrl}/api/blog/${blogID}`);
+  const blog = await res.json();
 
   return {
     props: {
-      blogs,
+      blog,
     },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const baseUrl = process.env.BASE_URL;
+  const res = await fetch(`${baseUrl}/api/blog`);
+  const blogs = await res.json();
+
+  const blogIDs = blogs.map((blog) => blog.id);
+  const paths = blogIDs.map((blogID) => ({
+    params: { id: blogID.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: false,
   };
 };
 
