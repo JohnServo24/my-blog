d0f9ec4c28c5efce2e580056cf9b29de0eb3a12b
john <jaservo@up.edu.ph>
2023-01-22 17:16:50 +0800

    Feat: Add ISR

--- utils/getAllBlogs.ts
+++ utils/getAllBlogs.ts
@@ -1,6 +1,6 @@
 const getAllBlogs = async () => {
   const baseUrl = process.env.BASE_URL;
-  const res = await fetch(`${baseUrl}/api/blog/`);
+  const res = await fetch(`${baseUrl}/api/blog/`, { next: { revalidate: 10 } });
   const resFormatted = await res.json();
   const blogs = resFormatted.data;
 
